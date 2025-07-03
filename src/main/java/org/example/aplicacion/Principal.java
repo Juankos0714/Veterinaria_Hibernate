d3261736d14845134a13d29171e1381b268eb5d4
Juankos0714 <juankos0714@gmail.com>
2025-07-02 23:07:39 -0500

    First commit

---
+++ src/main/java/org/example/aplicacion/Principal.java
@@ -0,0 +1,8 @@
+package org.example.aplicacion;
+
+public class Principal {
+    public static void main(String[] args) {
+        Aplicacion miAplicacion=new Aplicacion();
+        miAplicacion.iniciar();
+    }
+}
