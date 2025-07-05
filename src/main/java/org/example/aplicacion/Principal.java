c08cb173b2286aef9a2a26b27dd6344a4e92375c
Juankos0714 <juankos0714@gmail.com>
2025-07-05 00:55:07 -0500

    finish

--- src/main/java/org/example/aplicacion/Principal.java
+++ src/main/java/org/example/aplicacion/Principal.java
@@ -2,7 +2,6 @@ package org.example.aplicacion;
 
 public class Principal {
     public static void main(String[] args) {
-        Aplicacion miAplicacion=new Aplicacion();
-        miAplicacion.iniciar();
+        new Relaciones();
     }
 }
